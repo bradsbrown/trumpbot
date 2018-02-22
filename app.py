bff7168bece676667ff07cd22024d7c17ed80b6f
Brad Brown <brad.brown@rackspace.com>
2018-02-22 10:10:30 -0600

    init: getting started

---
+++ app.py
@@ -0,0 +1,14 @@
+import twitter_scraper
+import markovify
+
+
+def trump_tweets(pages=25):
+    return '\n'.join([t for t in twitter_scraper.get_tweets('realdonaldtrump', pages=pages)])
+
+
+def insanity(tweets):
+    return markovify.Text(tweets).make_short_sentences(280)
+
+
+if __name__ == '__main__':
+    print(insanity(trump_tweets()))
